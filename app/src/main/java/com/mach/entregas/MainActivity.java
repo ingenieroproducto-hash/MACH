45907a905bb37225d4a7010c401ebc0eb006706f
ingenieroproducto-hash <ingenieroproducto@yatla.com.mx>
2026-07-20 02:24:05 -0600

    Agregar app/src/main/java/com/mach/entregas/MainActivity.java

---
+++ app/src/main/java/com/mach/entregas/MainActivity.java
@@ -0,0 +1,91 @@
+package com.mach.entregas;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private static final int FILE_CHOOSER_REQUEST = 4102;
+    private WebView webView;
+    private ValueCallback<Uri[]> pendingFiles;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setStatusBarColor(Color.rgb(244, 246, 245));
+        getWindow().setNavigationBarColor(Color.WHITE);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setAllowContentAccess(true);
+        settings.setBuiltInZoomControls(false);
+        settings.setDisplayZoomControls(false);
+        settings.setLoadWithOverviewMode(true);
+        settings.setUseWideViewPort(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onShowFileChooser(
+                    WebView view,
+                    ValueCallback<Uri[]> filePathCallback,
+                    FileChooserParams fileChooserParams) {
+                if (pendingFiles != null) pendingFiles.onReceiveValue(null);
+                pendingFiles = filePathCallback;
+                Intent chooser;
+                try {
+                    chooser = fileChooserParams.createIntent();
+                } catch (Exception error) {
+                    pendingFiles = null;
+                    return false;
+                }
+                startActivityForResult(chooser, FILE_CHOOSER_REQUEST);
+                return true;
+            }
+        });
+
+        if (savedInstanceState == null) {
+            webView.loadUrl("file:///android_asset/index.html");
+        } else {
+            webView.restoreState(savedInstanceState);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode != FILE_CHOOSER_REQUEST || pendingFiles == null) return;
+        Uri[] result = resultCode == RESULT_OK
+                ? WebChromeClient.FileChooserParams.parseResult(resultCode, data)
+                : null;
+        pendingFiles.onReceiveValue(result);
+        pendingFiles = null;
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        webView.saveState(outState);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) webView.goBack();
+        else super.onBackPressed();
+    }
+}
